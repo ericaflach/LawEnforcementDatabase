59575ea71c92eeba44ae32b41c25b4364fcdc04c
Erica Flach <erica.flach@yahoo.com>
2021-03-11 09:18:44 -0500

    Added Victim Stub

---
+++ Victim.java
@@ -0,0 +1,30 @@
+public class Victim{
+
+    private String description;
+    private String contact;
+    private int victimID;
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getContact(){
+        return contact;
+    }
+
+    public int getID(){
+        return victimID;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setContact(String contact){
+        this.contact = contact;
+    }
+
+    public void setID(int ID){
+        this.victimID = ID;
+    }
+}
