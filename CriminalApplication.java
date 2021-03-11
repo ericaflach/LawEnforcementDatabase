56519ece1ec9018e74882ccdab122ae30da0137d
Ben Friend <ben@Bens-MacBook-Pro.local>
2021-03-11 12:14:43 -0500

    finished stubs

--- CriminalApplication.java
+++ CriminalApplication.java
@@ -1,5 +1,51 @@
 package LawEnforcementDatabase;
 
+import java.util.ArrayList;
+
 public class CriminalApplication {
-    
+
+    private PersonList personList;
+    private UserList userList;
+    private User user;
+
+    public CriminalApplication(){
+
+    }
+
+    public ArrayList<Criminals> searchCriminals(){
+
+    }
+
+    public void createPoliceOfficer(String firstName, String lastName, String username, String email, int phoneNumber, String password, int clearanceLevel, String department, int officerID){
+
+    }
+
+    public void createAdmin(String firstName, String lastName, String username, String email, int phoneNumber, String password, int clearanceLevel, int adminID){
+
+    }
+
+    public User login(String username, String password){
+
+    }
+
+    public ArrayList<Criminal> getMyCriminals(){
+
+    }
+
+    public ArrayList<Case> getMyCases(){
+
+    }
+
+    public Criminal searchCriminal(int criminalID){
+
+    }
+
+    public boolean addCriminal(String firstName, String lastName, int criminalID){
+
+    }
+
+    public void editCriminal(User creator, String attributes){
+        
+    }
 }
+
