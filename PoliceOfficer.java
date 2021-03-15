a150fc5871a8ca423130c73f8c625aaddb115b3a
Ben Friend <ben@Bens-MacBook-Pro.local>
2021-03-15 16:11:38 -0400

    wrote classes

--- PoliceOfficer.java
+++ PoliceOfficer.java
@@ -5,16 +5,32 @@ import java.util.ArrayList;
 public class PoliceOfficer extends User {
     
     private String policeDepartment;
-    private int OfficerID;
+    private int officerID;
     private ArrayList<Criminal> myCriminals;
     private ArrayList<Case> myCases;
+
+    public void setPoliceDepartment(String department){
+        this.policeDepartment = department;
+    }
+
+    public void setOfficerID(int officerID){
+        this.officerID=officerID;
+    }
+
+    public void setMyCriminals(ArrayList<Criminal> myCriminals){
+        this.myCriminals=myCriminals;
+    }
+
+    public setMyCases(ArrayList<Case> myCases){
+        this.myCases=myCases;
+    }
 
     public String getPoliceDepartment(){
         return policeDepartment;
     }
 
     public int getOfficerID(){
-        return OfficerID;
+        return officerID;
     }
 
     public ArrayList<Criminal> getMyCriminals(){
@@ -25,7 +41,5 @@ public class PoliceOfficer extends User {
         return myCases;
     }
 
-    public void setPoliceDepartment(String department){
-      this.policeDepartment = department;
-    }
+    
 }
