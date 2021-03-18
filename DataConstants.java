e048650127dda6edec9753dd598b339fa94ee5ae
Cphoward484 <cphoward@email.uscb.edu>
2021-03-17 21:26:47 -0400

    creating DataConstants - example template code

--- DataConstants.java
+++ DataConstants.java
@@ -1,3 +1,9 @@
-public class DataConstants {
+public abstract class DataConstants {
+
+        protected static final String PEOPLE_FILE_NAME = "src/people.json";
+        protected static final String PEOPLE = "people";
+        protected static final String PEOPLE_FIRST_NAME = "firstName";
+        protected static final String PEOPLE_LAST_NAME = "lastName";
+        protected static final String PEOPLE_PHONE_NUMBER = "phoneNumber";
     
 }
