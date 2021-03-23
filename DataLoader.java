338d2f2c5ac92d394d00fb18d24ebd5b7def70df
Erica Flach <erica.flach@yahoo.com>
2021-03-23 15:45:19 -0400

    Added getCases

--- DataLoader.java
+++ DataLoader.java
@@ -6,33 +6,34 @@ import java.util.UUID;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-public class DataLoader extends DataConstants{
-    static ArrayList<Person> people = new ArrayList<Person>();
+public class DataLoader extends DataConstants {
 
-    public static ArrayList<Person> getPersons(){
+    public static ArrayList<Person> getPersons() {
+        ArrayList<Person> people = new ArrayList<Person>();
 
         try {
-            readCriminal();
-            readPersonOfInterest();
-            readSuspect();
-            readVictim();
+            people.addAll(readCriminal());
+            people.addAll(readPersonOfInterest());
+            people.addAll(readSuspect());
+            people.addAll(readVictim());
             
             return people;
         }
 
-        catch(Exception e){
+        catch (Exception e) {
             e.printStackTrace();
         }
 
         return null;
     }
 
-    public static ArrayList<Person> readCriminal(){
-        FileReader reader = new FileReader("src/criminal.json");
+    public static ArrayList<Person> readCriminal() {
+        FileReader reader = new FileReader("src/Criminal.json");
         JSONParser parser = new JSONParser();
         JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+        ArrayList<Person> people = new ArrayList<Person>();
 
-        for(int i = 0; i < peopleJSON.size(); i++){
+        for (int i = 0; i < peopleJSON.size(); i++) {
             JSONObject personJSON = (JSONObject)peopleJSON.get(i);
             String firstName = (String)personJSON.get("firstName");
             String lastName = (String)personJSON.get("lastName");
@@ -50,15 +51,19 @@ public class DataLoader extends DataConstants{
             
             people.add(new Criminal(firstName, lastName, age, dateOfBirth, creator, pastCrimes, address,
             shoeSize, priority, alive, physicalAttributes, inJail, criminalID));
+
         }
+
+        return people;
     }
 
-    public static ArrayList<Person> readPersonOfInterest(){
-        FileReader reader = new FileReader("src/personOfInterest.json");
+    public static ArrayList<Person> readPersonOfInterest() {
+        FileReader reader = new FileReader("src/PersonOfInterest.json");
         JSONParser parser = new JSONParser();
         JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+        ArrayList<Person> people = new ArrayList<Person>();
 
-        for(int i = 0; i < peopleJSON.size(); i++){
+        for (int i = 0; i < peopleJSON.size(); i++) {
             JSONObject personJSON = (JSONObject)peopleJSON.get(i);
             String firstName = (String)personJSON.get("firstName");
             String lastName = (String)personJSON.get("lastName");
@@ -70,14 +75,17 @@ public class DataLoader extends DataConstants{
             
             people.add(new PersonOfInterest(firstName, lastName, age, dateOfBirth, description, contact, id));
         }
+
+        return people;
     }
     
-    public static ArrayList<Person> readSuspect(){
-        FileReader reader = new FileReader("src/suspect.json");
+    public static ArrayList<Person> readSuspect() {
+        FileReader reader = new FileReader("src/Suspect.json");
         JSONParser parser = new JSONParser();
         JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+        ArrayList<Person> people = new ArrayList<Person>();
 
-        for(int i = 0; i < peopleJSON.size(); i++){
+        for (int i = 0; i < peopleJSON.size(); i++) {
             JSONObject personJSON = (JSONObject)peopleJSON.get(i);
             String firstName = (String)personJSON.get("firstName");
             String lastName = (String)personJSON.get("lastName");
@@ -86,56 +94,121 @@ public class DataLoader extends DataConstants{
             String alibi = (String)personJSON.get("alibi");
             int id = ((long)personJSON.get("suspectID")).intValue();
             
-            people.add(new PersonOfInterest(firstName, lastName, age, dateOfBirth, alibi, id));
+            people.add(new Suspect(firstName, lastName, age, dateOfBirth, alibi, id));
         }
+
+        return people;
     }
 
-    public static ArrayList<Person> readVictim(){
-        FileReader reader = new FileReader("src/victim.json");
+    public static ArrayList<Person> readVictim() {
+        FileReader reader = new FileReader("src/Victim.json");
         JSONParser parser = new JSONParser();
         JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+        ArrayList<Person> people = new ArrayList<Person>();
 
-        for(int i = 0; i < peopleJSON.size(); i++){
+        for (int i = 0; i < peopleJSON.size(); i++) {
             JSONObject personJSON = (JSONObject)peopleJSON.get(i);
             String firstName = (String)personJSON.get("firstName");
             String lastName = (String)personJSON.get("lastName");
-            int age = ((long)personJSON.get("age")).intValue();
+            int age = (int)personJSON.get("age"));
             String dateOfBirth = (String)personJSON.get("dateOfBirth");
             String description = (String)personJSON.get("description");
             String contact = (String)personJSON.get("contact");
-            int id = ((long)personJSON.get("victimID")).intValue();
+            int id = (int)personJSON.get("victimID");
             
-            people.add(new PersonOfInterest(firstName, lastName, age, dateOfBirth, description, contact, id));
+            people.add(new Victim(firstName, lastName, age, dateOfBirth, description, contact, id));
         }
+
+        return people;
     }
 
-    public static ArrayList<User> getUsers(){
+    public static ArrayList<User> getUsers() {
         ArrayList<User> users = new ArrayList<User>();
+
         try {
-            FileReader reader = new FileReader(USER_FILE_NAME);
-            JSONParser parser = new JSONParser();
-            JSONArray usersJSON = (JSONArray)new JSONParser().parse(reader);
-
-            for(int i = 0; i < usersJSON.size(); i++){
-                JSONObject userJSON = (JSONObject)usersJSON.get(i);
-                String firstName = (String)usersJSON.get(USER_FIRST_NAME);
-				String lastName = (String)usersJSON.get(USER_LAST_NAME);
-				String username = (String)usersJSON.get(USER_USERNAME);
-				String password = (String)usersJSON.get(USER_PASSWORD);
-                String email = (String)usersJSON.get(USER_EMAIL);
-				int phoneNumber = (int)usersJSON.get(USER_PHONE_NUMBER);
-				int clearanceLevel = (int)usersJSON.get(USER_CLEARANCE_LEVEL);
-
-				users.add(new User(firstName, lastName, username, password, email, phoneNumber, clearanceLevel));
-            }
+            users.addAll(readCreateUser());
+            users.addAll(readUpdateUser());
             
             return users;
         }
 
-        catch(Exception e){
+        catch (Exception e) {
             e.printStackTrace();
         }
 
         return null;
+    }
+
+    public static ArrayList<User> readCreateUser() {
+        FileReader reader = new FileReader("src/CreateUser.json");
+        JSONParser parser = new JSONParser();
+        JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+        ArrayList<User> users = new ArrayList<User>();
+
+            for (int i = 0; i < peopleJSON.size(); i++) {
+                JSONObject personJSON = (JSONObject)peopleJSON.get(i);
+                String firstName = (String)personJSON.get(USER_FIRST_NAME);
+				String lastName = (String)personJSON.get(USER_LAST_NAME);
+				String username = (String)personJSON.get(USER_USERNAME);
+				String password = (String)personJSON.get(USER_PASSWORD);
+                String email = (String)personJSON.get(USER_EMAIL);
+				int phoneNumber = (int)personJSON.get(USER_PHONE_NUMBER);
+				int clearanceLevel = (int)personJSON.get(USER_CLEARANCE_LEVEL);
+
+				users.add(new User(firstName, lastName, username, password, email, phoneNumber, clearanceLevel));
+            }
+
+        return users;
+    }
+
+    public static ArrayList<User> readUpdateUser() {
+        FileReader reader = new FileReader("src/UpdateUser.json");
+        JSONParser parser = new JSONParser();
+        JSONArray peopleJSON = (JSONArray)new JSONParser().parse(reader);
+        ArrayList<User> users = new ArrayList<User>();
+
+            for (int i = 0; i < peopleJSON.size(); i++) {
+                JSONObject personJSON = (JSONObject)peopleJSON.get(i);
+                String firstName = (String)personJSON.get(USER_FIRST_NAME);
+				String lastName = (String)personJSON.get(USER_LAST_NAME);
+				String username = (String)personJSON.get(USER_USERNAME);
+				String password = (String)personJSON.get(USER_PASSWORD);
+                String email = (String)personJSON.get(USER_EMAIL);
+				int phoneNumber = (int)personJSON.get(USER_PHONE_NUMBER);
+				int clearanceLevel = (int)personJSON.get(USER_CLEARANCE_LEVEL);
+
+				users.add(new User(firstName, lastName, username, password, email, phoneNumber, clearanceLevel));
+            }
+
+        return users;
+    }
+
+    public static ArrayList<Case> getCases() {
+        ArrayList<Case> users = new ArrayList<User>();
+		
+		try {
+			FileReader reader = new FileReader("src/Cases.json");
+			JSONParser parser = new JSONParser();	
+			JSONArray casesJSON = (JSONArray)new JSONParser().parse(reader);
+			
+			for(int i=0; i < casesJSON.size(); i++) {
+				JSONObject caseJSON = (JSONObject)casesJSON.get(i);
+				/*UUID id = UUID.fromString((String)caseJSON.get(USER_ID));
+				String userName = (String)caseJSON.get(USER_USER_NAME);
+				String firstName = (String)caseJSON.get(USER_FIRST_NAME);
+				String lastName = (String)caseJSON.get(USER_LAST_NAME);
+				int age = ((Long)caseJSON.get(USER_AGE)).intValue();
+				String phoneNumber = (String)caseJSON.get(USER_PHONE_NUMBER);
+				
+				users.add(new User(id, userName, firstName, lastName, age, phoneNumber));*/
+			}
+			
+			return users;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
     }
 }
