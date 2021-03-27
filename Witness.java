ff1a4f7760c04a7b37df7e0d089c0223081748b7
chrisnelson01 <cdn2@email.sc.edu>
2021-03-27 16:49:14 -0400

    running version with javadocs templates
    Co-authored-by: Cphoward484 <Cphoward484@users.noreply.github.com>

--- Witness.java
+++ Witness.java
@@ -1,10 +1,18 @@
 package LawEnforcementDatabase;
 
 public class Witness extends Person{
-    
+   
+     /**
+     * This method 
+     * @return 
+     */
     private String description;
     private String contact;
 
+     /**
+     * This method 
+     * @return 
+     */
     public Witness(String firstName, String lastName, int age, String DOB, int ID,
      char sex, String description, String contact) {
         super(firstName, lastName, age, DOB, ID, sex);
@@ -13,22 +21,42 @@ public class Witness extends Person{
         this.contact = contact;
     }
 
+     /**
+     * This method 
+     * @return 
+     */
     public String getDescription() {
         return description;
     }
 
+     /**
+     * This method 
+     * @return 
+     */
     public String getContact() {
         return contact;
     }
 
+     /**
+     * This method 
+     * @return 
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+     /**
+     * This method setContact 
+     * @return the unique attribute value Contact
+     */
     public void setContact(String contact) {
         this.contact = contact;
     }
 
+     /**
+     * This method toString contains the listed objects 
+     * @return the unique conditions values.
+     */
     public String toString() {
         return super.toString()
         + "\nDescription: " + description
