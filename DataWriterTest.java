921b4f81137c38e0fcf544f45f1659c0b68e321e
Cphoward484 <cphoward@email.uscb.edu>
2021-04-10 19:31:31 -0400

    currently uplaoding template code

--- DataWriterTest.java
+++ DataWriterTest.java
@@ -1,5 +1,35 @@
 package LawEnforcementDatabase;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DataWriterTest {
-    
+    private User user = User.getInstance();
+    private ArrayList<User> userList = user.getUser();
+
+    @BeforeEach
+    public void setup(){
+        Users.getInstance().getUser()().clear();
+        DataWriter.saveUser();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        User.getInstance().getUser().clear();
+        DataWriter.saveUser();
+    }
+    @Test
+    void testWritingZeroUsers(){
+        userList = DataLoader.getUser();
+        assertEquals(0, userList.size());
+    }
+    @Test
+    void testWritingOneUser(){
+    User.getInstance().getUser().clear();
+    assertEquals(0, userList.size());
+    }
+    @Test
+    void testWritingFiveUsers(){
+        userList = DataLoader.getUser();
+        assertEquals("jturner", userList.get(0).getUserName());
+    }
 }
